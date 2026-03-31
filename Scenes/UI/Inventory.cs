0c1c3d93e53540e9e0c3669e9007b23fce0b63a0
Alston Rodriguez <avrodri3@ncsu.edu>
2026-03-31 17:26:06 -0400

    added methods for adding, removing, and checking for an item

--- Scenes/UI/Inventory.cs
+++ Scenes/UI/Inventory.cs
@@ -1,6 +1,52 @@
 using Godot;
-using System;
+using System.Collections.Generic;
 
-public partial class InventoryNewNew : Control
+public partial class Inventory : Control
 {
+    
+    private class InventoryItem
+    {
+        public int count { get; set; }
+        public int index { get; set; }
+    }
+    
+    private Dictionary<Item, InventoryItem> _lookup = new();
+    private List<InventoryItem> _contents = new();
+    
+    public void ClearInventory()
+    {
+        _contents.Clear();
+    }
+    
+    public void AddItem(Item item)
+    {
+        if (item == null) return;
+        if (_lookup.TryGetValue(item, out var inventoryItem)) inventoryItem.count++;
+        else {
+            var newItem = new InventoryItem { count = 1, index = _contents.Count };
+            _contents.Add(newItem);
+            _lookup[item] = newItem;
+        }
+    }
+    
+    public void RemoveItem(Item item)
+    {
+        if (item == null) return;
+        if (!_lookup.TryGetValue(item, out var inventoryItem)) return;
+        
+        inventoryItem.count--;
+        if (inventoryItem.count <= 0) {
+            _lookup.Remove(item);
+            _contents.Remove(inventoryItem);
+            
+            for (int i = 0; i < _contents.Count; i++) {
+                _contents[i].index = i;
+            }
+        }
+    }
+    
+    public bool HasItem(Item item)
+    {
+        return item != null && _lookup.ContainsKey(item);
+    }
 }
