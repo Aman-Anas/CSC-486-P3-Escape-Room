d2ed95eadecf36ce41cb5b9b5d2e17b5086cdb1d
Alston Rodriguez <avrodri3@ncsu.edu>
2026-04-06 23:10:52 -0400

    test scene, script planning

--- Game/Entities/CipherPuzzle.cs
+++ Game/Entities/CipherPuzzle.cs
@@ -3,4 +3,8 @@ using System;
 
 public partial class CipherPuzzle : Node3D
 {
+    // static cipher key
+    // instance of ciphertext
+    // method for rotating a layer
+    // method for checking validity of cipher
 }
