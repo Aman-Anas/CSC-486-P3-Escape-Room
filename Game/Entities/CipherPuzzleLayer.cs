66a97c659a3cd362a676d7a8ff21242329ac209b
Alston Rodriguez <avrodri3@ncsu.edu>
2026-04-07 01:16:16 -0400

    naming conventions, randomizing index

--- Game/Entities/CipherPuzzleLayer.cs
+++ Game/Entities/CipherPuzzleLayer.cs
@@ -1,55 +1,65 @@
 using Godot;
 using System;
-using System.Collections.Generic;
+// using System.Collections.Generic;
 
 public partial class CipherPuzzleLayer : Node3D
 {
     [Export] public CompressedTexture2D[] RuneTextures = [];
-    [Export] public Color RuneColor = new Color("#7f543d");
-    
-    private int rotationIndex = 0;
-    private int selected = 0;
-    private double runeDistance = 1.01;
-    
-    private void _ClampSelected()
+    [Export] public Color RuneColor = new("#7f543d");
+
+    private static readonly Random random = new();
+
+    private int _rotationIndex = 0;
+    public int SelectionIndex { get; private set; } = 0;
+    private double _runeDistance = 1.01;
+
+    public int RandomizeSelection()
     {
-        selected = (selected + RuneTextures.Length) % RuneTextures.Length;
+        SelectionIndex = random.Next(RuneTextures.Length);
+        _rotationIndex = SelectionIndex;
+        DoRotation();
+        return SelectionIndex;
     }
-    
-    private void _PrintSelected()
+
+    private void ClampSelected()
     {
-        GD.Print($"selected: {selected}");
+        SelectionIndex = (SelectionIndex + RuneTextures.Length) % RuneTextures.Length;
     }
-    
-    private void _DoRotation()
+
+    private void PrintSelected()
+    {
+        GD.Print($"selected: {SelectionIndex}");
+    }
+
+    private void DoRotation()
     {
         Tween tween = CreateTween();
-        float target = (float)rotationIndex / RuneTextures.Length * 2.0f * (float)Math.PI;
+        float target = (float)_rotationIndex / RuneTextures.Length * 2.0f * (float)Math.PI;
         tween.TweenProperty(this, "rotation:y", target, 0.5f)
             .SetTrans(Tween.TransitionType.Quart)
             .SetEase(Tween.EaseType.Out);
     }
-    
+
     public void RotateLeft()
     {
-        rotationIndex--;
-        _DoRotation();
-        
-        selected--;
-        _ClampSelected();
-        _PrintSelected();
+        _rotationIndex--;
+        DoRotation();
+
+        SelectionIndex--;
+        ClampSelected();
+        PrintSelected();
     }
-    
+
     public void RotateRight()
     {
-        rotationIndex++;
-        _DoRotation();
-            
-        selected++;
-        _ClampSelected();
-        _PrintSelected();
+        _rotationIndex++;
+        DoRotation();
+
+        SelectionIndex++;
+        ClampSelected();
+        PrintSelected();
     }
-    
+
     public override void _Ready()
     {
         // create runes around edge
@@ -58,7 +68,7 @@ public partial class CipherPuzzleLayer : Node3D
             Sprite3D sprite = new Sprite3D();
             sprite.Texture = RuneTextures[i];
             sprite.Modulate = RuneColor;
-            
+
             // set position
             double angle = (double)i / RuneTextures.Length * 2 * Math.PI;
             sprite.Position = new Vector3(
@@ -66,15 +76,16 @@ public partial class CipherPuzzleLayer : Node3D
                 0,
                 (float)Math.Cos(angle)
             );
-            
+
             // set rotation
-            sprite.Rotation = sprite.Rotation with {
+            sprite.Rotation = sprite.Rotation with
+            {
                 Y = (float)angle
             };
-            
+
             // set scale
             sprite.Scale *= (float)0.28;
-            
+
             AddChild(sprite);
         }
     }
